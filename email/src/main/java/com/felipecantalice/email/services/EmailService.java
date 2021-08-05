e52a2e1834cde3670b99efc93364543628482fb9
Felipe Cantalice <felipep31a@gmail.com>
2021-08-05 17:00:19 -0400

    Email service
    método para enviar email

--- email/src/main/java/com/felipecantalice/email/services/EmailService.java
+++ email/src/main/java/com/felipecantalice/email/services/EmailService.java
@@ -1,9 +1,14 @@
 package com.felipecantalice.email.services;
 
+import com.felipecantalice.email.enums.StatusEmail;
 import com.felipecantalice.email.models.EmailModel;
 import com.felipecantalice.email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -11,6 +16,24 @@ public class EmailService {
     @Autowired
     private EmailRepository emailRepository;
 
-    public void sendEmail(EmailModel model) {
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SEND);
+        } catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
     }
 }
