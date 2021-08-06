fd5bca0a84bada3607b44a277b23bacbf296b061
Felipe Cantalice <felipep31a@gmail.com>
2021-08-06 07:52:03 -0400

    Email Model Long para UUID
    Troca do identificar do modelo Email

--- email/src/main/java/com/felipecantalice/email/models/EmailModel.java
+++ email/src/main/java/com/felipecantalice/email/models/EmailModel.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @Entity
@@ -14,7 +15,7 @@ public class EmailModel implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private UUID id;
 
     private String ownerRef;
     private String emailFrom;
--- email/src/main/java/com/felipecantalice/email/repositories/EmailRepository.java
+++ email/src/main/java/com/felipecantalice/email/repositories/EmailRepository.java
@@ -3,5 +3,7 @@ package com.felipecantalice.email.repositories;
 import com.felipecantalice.email.models.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailRepository extends JpaRepository<EmailModel,Long> {
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 }
